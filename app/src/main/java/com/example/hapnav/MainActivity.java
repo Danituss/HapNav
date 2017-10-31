0859843eb04e010902edbb6e688fe94d202293c0
Danituss <dan.huiko14@gmail.com>
2017-10-31 14:00:12 +0200

    Map activity

--- app/src/main/java/com/example/hapnav/MainActivity.java
+++ app/src/main/java/com/example/hapnav/MainActivity.java
@@ -11,11 +11,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+    */
 //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +33,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     @Override
@@ -59,10 +56,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
